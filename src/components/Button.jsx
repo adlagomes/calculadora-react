87ae028cb3ec0940391a0314c52a6bd3cd1e9fa7
Adla Gomes <81967816+adlagomes@users.noreply.github.com>
2021-10-31 07:46:26 -0300

    Add files via upload

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import './Button.css'
+import "./Button.css";
 
-const button = props => {
-  let classes = 'button '
-  classes += props.operation ? 'operation' : ''
-  classes += props.double ? 'double' : ''
-  classes += props.triple ? 'triple' : ''
+const button = (props) => {
+  let classes = "button ";
+  classes += props.operation ? "operation" : "";
+  classes += props.double ? "double" : "";
+  classes += props.triple ? "triple" : "";
 
   return (
-    <button className={classes}
-      onClick={e => props.click && props.click(props.label)}>
+    <button
+      className={classes}
+      onClick={(e) => props.click && props.click(props.label)}
+    >
       {props.label}
     </button>
-  )
-}
+  );
+};
 
-export default button
+export default button;
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,40 +1,72 @@
-import React, { Component } from 'react'
-import Button from '../components/Button'
-import Display from '../components/Display'
-import './Calculator.css'
+import React, { Component } from "react";
+import Button from "../components/Button";
+import Display from "../components/Display";
+import "./Calculator.css";
 
 const initialState = {
-  displayValue: '0',
+  displayValue: "0",
   clearDisplay: false,
   operation: null,
   values: [0, 0],
-  current: 0
-}
+  current: 0,
+};
 
 export default class Calculator extends Component {
-
-  state = { ...initialState }
+  state = { ...initialState };
 
   clearMemory() {
-    this.setState({ ...initialState })
+    this.setState({ ...initialState });
   }
 
   setOperation(operation) {
-    console.log(operation)
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const result = operation === "=";
+      const currentOperation = this.state.operation;
+
+      const values = [...this.state.values];
+      values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+      values[1] = 0
+
+      this.setState({
+        displayValue: values[0],
+        operation: result ? null : operation,
+        current: result ? 0 : 1,
+        clearDisplay: !result,
+        values
+      })
+    }
   }
 
   addDigit(n) {
-    console.log(n)
+    if (n === "." && this.state.displayValue.includes(".")) {
+      return;
+    }
+
+    const clearDisplay =
+      this.state.displayValue === "0" || this.state.clearDisplay;
+    const currentValue = clearDisplay ? "" : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({ displayValue, clearDisplay: false });
+
+    if (n !== ".") {
+      const i = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[i] = newValue;
+      this.setState({ values });
+    }
   }
 
   render() {
-    const addDigit = n => this.addDigit(n)
-    const setOperation = op => this.setOperation(op)
+    const addDigit = (n) => this.addDigit(n); // Esse this. presente dentro do render representar o objeto atual.
+    const setOperation = (op) => this.setOperation(op);
     return (
       <div className="Calculator">
         <Display value={this.state.displayValue} />
         <Button label="AC" click={() => this.clearMemory()} triple />
-        <Button label="/" click={this.setOperation} operation />
+        <Button label="/" click={setOperation} operation />
         <Button label="7" click={addDigit} />
         <Button label="8" click={addDigit} />
         <Button label="9" click={addDigit} />
@@ -51,6 +83,6 @@ export default class Calculator extends Component {
         <Button label="." click={addDigit} />
         <Button label="=" click={setOperation} operation />
       </div>
-    )
+    );
   }
 }
